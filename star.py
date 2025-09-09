586c60fc1626ec421d38e748aaa86804459d9f92
doongju <doongju1@gmail.com>
2025-09-09 11:48:41 +0900

    수정

---
+++ star.py
@@ -0,0 +1,15 @@
+class Pyramid:
+    def __init__(self, height):
+        self.height = height
+
+    def print_reverse_star_pyramid(self):
+        for i in range(self.height, 0, -1):
+            print(" " * (self.height - i) + "*" * (2 * i - 1))
+
+
+height = int(input("피라미드 높이를 입력하세요: "))
+p = Pyramid(height)
+p.print_reverse_star_pyramid()
+
+
+#commit 
